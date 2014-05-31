f2b87e49780f32a48588956f4ebf00ea41734e2e
Tom Reznick <thomas.reznick@gmail.com>
2014-05-31 15:58:58 -0400

    updates bookService for more crud, file operations

--- src/main/java/com/luckypants/books/BookService.java
+++ src/main/java/com/luckypants/books/BookService.java
@@ -1,21 +1,24 @@
 package com.luckypants.books;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.io.InputStream;
 
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.DELETE;
 
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
-import javax.ws.rs.FormParam;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.QueryParam;
 
 import javax.ws.rs.Produces;
 import javax.ws.rs.Consumes;
 
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response.ResponseBuilder;
 
 import org.codehaus.jackson.map.ObjectMapper;
 
@@ -23,8 +26,10 @@ import com.luckypants.command.CreateBookCommand;
 import com.luckypants.command.DeleteBookCommand;
 import com.luckypants.command.GetBookCommand;
 import com.luckypants.command.ListAllBooksCommand;
+import com.luckypants.command.ProvidePackagedFileCommand;
 import com.luckypants.command.FindBooksCommand;
 import com.luckypants.model.Book;
+import com.luckypants.properties.PropertiesLookup;
 import com.mongodb.DBObject;
 
 
@@ -32,6 +37,7 @@ import com.mongodb.DBObject;
 public class BookService {
 	ObjectMapper mapper = new ObjectMapper();
 	
+//	List books
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response listBooks() {
@@ -40,7 +46,17 @@ public class BookService {
 		return Response.status(200).entity(list).build();
 	}
 	
-//	Search Logic
+//	Get Property
+	@GET
+	@Path("properties/{property}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response propertiesCheck(@PathParam("property") String property) {
+		PropertiesLookup pl = new PropertiesLookup();
+		String pValue = pl.getProperty(property);
+		return Response.status(200).entity(pValue).build();
+	}
+	
+//	Get Book by key and Value
 	@GET
 	@Path("/{key}/{value}")
 	@Produces(MediaType.APPLICATION_JSON)
@@ -50,6 +66,27 @@ public class BookService {
 		return Response.status(200).entity(list).build();
 	}
 	
+//	Search-logic
+	@GET
+	@Path("/search")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response searchBook(
+			@DefaultValue("*") @QueryParam("query") String query,
+			@DefaultValue("author") @QueryParam("sortby") String sortby) {
+		HashMap<String, String> responseMap = new HashMap<String, String>();
+		responseMap.put("query", query);
+		responseMap.put("sortby", sortby);
+		String rString = "";
+		try {
+			rString = mapper.writeValueAsString(responseMap);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return Response.status(500).entity(e.toString()).build();
+		}
+		return Response.status(200).entity(rString).build();
+	}
+	
+//	isbn-based-crud
 	@GET
 	@Path("/{isbn}")
 	@Produces(MediaType.APPLICATION_JSON)
@@ -90,6 +127,40 @@ public class BookService {
 			return Response.status(500).entity("Error: Could not delete "+isbn).build();
 		}
 	}
-		
+	
+//	File operations
+	@GET
+	@Path("files/{filename}")
+	@Produces(MediaType.WILDCARD)
+	public Response getFile(@PathParam("filename") String filename) {
+		try {
+			ProvidePackagedFileCommand getFile = new ProvidePackagedFileCommand();
+			InputStream is = getFile.execute(filename);
+
+			ResponseBuilder response = Response.ok((Object) is);
+			response.header("Content-Disposition", "attachment; filename=\""
+					+ filename + "\"");
+			return response.build();
+		} catch (Exception e) {
+			return Response.status(404).entity(e.getMessage()).build();
+		}
+	}
+
+	@GET
+	@Path("inline/{filename}")
+	@Produces(MediaType.WILDCARD)
+	public Response renderFile(@PathParam("filename") String filename) {
+		try {
+			ProvidePackagedFileCommand getFile = new ProvidePackagedFileCommand();
+			InputStream is = getFile.execute(filename);
+
+			ResponseBuilder response = Response.ok((Object) is);
+			response.header("Content-Disposition", "inline; filename=\""
+					+ filename + "\"");
+			return response.build();
+		} catch (Exception e) {
+			return Response.status(404).entity(e.getMessage()).build();
+		}
+	}
 
 }
