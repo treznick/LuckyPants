9ea77f2eec9ef436c0ff14f327c87931bfd5a30b
Tom Reznick <thomas.reznick@gmail.com>
2014-06-06 17:06:18 -0400

    adds AuthorService

---
+++ src/main/java/com/luckypants/books/AuthorService.java
@@ -0,0 +1,35 @@
+package com.luckypants.books;
+
+import java.util.ArrayList;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.MediaType;
+
+import org.codehaus.jackson.map.ObjectMapper;
+
+import com.luckypants.command.ListAllAuthorsCommand;
+import com.mongodb.DBObject;
+
+@Path("/authors")
+public class AuthorService {
+	ObjectMapper mapper = new ObjectMapper();
+	
+//	List authors
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response listAuthors() {
+		ListAllAuthorsCommand listAuthors = new ListAllAuthorsCommand();
+		ArrayList<DBObject> list = listAuthors.execute();
+		String authorString = null;
+		try {
+			authorString = mapper.writeValueAsString(list);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return Response.status(200).entity(authorString).build();
+	}
+
+}
