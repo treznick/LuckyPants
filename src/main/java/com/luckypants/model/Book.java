ab2875c49a9b07db79096680cdee9ff1cd1ec04d
Tom Reznick <thomas.reznick@gmail.com>
2014-06-06 15:48:51 -0400

    updates Book model to reflect denormalized Author and foreign key author reference

--- src/main/java/com/luckypants/model/Book.java
+++ src/main/java/com/luckypants/model/Book.java
@@ -1,14 +1,23 @@
 package com.luckypants.model;
 
+import java.util.ArrayList;
+
+import org.codehaus.jackson.annotate.JsonIgnore;
 import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+import org.codehaus.jackson.map.annotate.JsonDeserialize;
+
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 
 public class Book {
+	
+	@JsonIgnore private Author author;
 	private String title;
 	private int pages;
-	private String author;
+	private String _author_id;
 	private String isbn;
+	 @JsonDeserialize(as=ArrayList.class, contentAs=String.class)
+	private ArrayList<String> genres;
 
 	public String getISBN() {
 		return isbn;
@@ -26,11 +35,11 @@ public class Book {
 		this.title = title;
 	}
 
-	public String getAuthor() {
+	public Author getAuthor() {
 		return author;
 	}
 	
-	public void setAuthor(String author) {
+	public void setAuthor(Author author) {
 		this.author = author;
 	}
 	
@@ -40,5 +49,21 @@ public class Book {
 	
 	public void setPages(int pages) {
 		this.pages = pages;
+	}
+	
+	public ArrayList<String> getGenres() {
+		return genres;
+	}
+	 @JsonDeserialize(as=ArrayList.class, contentAs=String.class)
+	public void setGenres(ArrayList<String> genres) {
+		this.genres = genres;
+	}
+	
+	public String get_author_id() {
+		return _author_id;
+	}
+
+	public void set_author_id(String _author_id) {
+		this._author_id = _author_id;
 	}
 }
